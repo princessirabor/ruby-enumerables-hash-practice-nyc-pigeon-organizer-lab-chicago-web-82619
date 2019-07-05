17943e1be7c7c406f761574582880cb320e180ed
Princess Irabor <irabor.princess1@gmail.com>
2019-07-05 20:06:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
       
       keyupdater2 =key2
       value2.each do |thisvalue2|
-        p "#{resultHash[thisvalue2]} i hit here"
+        p "#{!!resultHash[thisvalue2]} i hit here"
         if !!resultHash[thisvalue2]
           newArray.push(keyupdater2);
           #result2Hash[keyupdater] = newArray.push(keyupdater2)
