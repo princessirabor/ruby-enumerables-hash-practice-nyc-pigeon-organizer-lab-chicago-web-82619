ba90f777d0b29fb7fe0bfae40b0b3e3817e098eb
Princess Irabor <irabor.princess1@gmail.com>
2019-07-05 20:45:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  resultHash = {}
+  result2Hash ={}
+  tempHash={}
+  keyupdater =""
+  keyupdater2 =""
+  keyupdater3 =""
+  newArray = []
+  
+  data.each do |(key,value)|
+    keyupdater = key
+    value.each do |(key2,value2)|
+      keyupdater2 =key2
+      value2.each do |thisvalue2|
+        if !!!resultHash[thisvalue2]
+          keyupdater3 = keyupdater2.to_s 
+          newArray.push(keyupdater3);
+        else
+          if resultHash[thisvalue2][keyupdater] == nil
+          result2Hash = resultHash[thisvalue2];
+          keyupdater3 = keyupdater2.to_s 
+          newArray.push(keyupdater3);
+          else
+            newArray = resultHash[thisvalue2][keyupdater]
+            keyupdater3 = keyupdater2.to_s 
+            newArray.push(keyupdater3);
+          end
+        end
+        result2Hash[keyupdater] =newArray;
+        resultHash[thisvalue2] = result2Hash
+        newArray =[];
+        result2Hash ={};
+      end
+    end
+  end
+  resultHash
 end
